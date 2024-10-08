00dde597a5d65f5017064daf605fc8795b5952cf
CarlosMVergara <156954736+CarlosMVergara@users.noreply.github.com>
2024-10-08 15:55:25 -0600

    Add files via upload

---
+++ src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,14 @@
+package ejercicio4;
+
+public class Ejercicio4 {
+
+
+    public static void main(String[] args) {
+        
+        for(int i = 20; i <= 200; i+=2){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
